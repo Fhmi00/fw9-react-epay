7067b8f2e4b2fce595affc0b68ed0ba36ba26cb8
Fhmi00 <anisfahmi045@gmail.com>
2022-07-28 16:46:49 +0700

    add: components footer.js

---
+++ src/assets/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="d-flex justify-content-between mt-2 py-4 footer">
+            <span className="footer-copyright">2020 Zwallet. All right reserved.</span>
+            <div className="d-flex gap-5 footer-contact">
+                <span>+62 5637 8882 9901</span>
+                <span>contact@zwallet.com</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
